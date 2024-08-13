024b81363633599cee3bd6e61550ef23717e0bdd
brobgonal <97367267+hybhens@users.noreply.github.com>
2024-08-13 18:26:10 +0200

    Create bandpass-effect.js

---
+++ src/lib/audio/effects/bandpass-effect.js
@@ -0,0 +1,21 @@
+class BandPassEffect {
+    constructor (audioContext, startSeconds, endSeconds) {
+        this.audioContext = audioContext;
+
+        this.input = this.audioContext.createGain();
+        this.output = this.audioContext.createGain();
+
+        this.effect = this.audioContext.createBiquadFilter();
+        this.effect.type = "bandpass";
+        this.effect.frequency.value = 11025;
+        this.effect.Q.value = 0.7;
+
+        this.effect.frequency.setValueAtTime(880, startSeconds);
+        this.effect.frequency.setValueAtTime(11025, endSeconds);
+
+        this.input.connect(this.effect);
+        this.effect.connect(this.output);
+    }
+}
+
+export default BandPassEffect;
